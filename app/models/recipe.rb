753a39366824a4351c7e6317158dba9f217b4af1
Jul Jen <juliajen.prog@gmail.com>
2018-07-19 04:56:16 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,7 +8,7 @@ class Recipe < ActiveRecord::Base
   end
 
   def ingredient_attributes=(ingredient)
-   self.ingredient = Ingredient.find_or_create_by(name: ingredient.name)
-   self.ingredient.update(ingredient)
+    self.ingredient = Ingredient.find_or_create_by(name: ingredient.name)
+    self.ingredient.update(ingredient)
   end
 end
