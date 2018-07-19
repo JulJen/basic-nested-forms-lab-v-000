524b4e4277d1590bf63cfebefd5b99ca870f4a46
Jul Jen <juliajen.prog@gmail.com>
2018-07-19 06:05:52 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    # @recipe.ingredients.build
-    # @recipe.ingredients.build
+    @recipe.ingredients.build
+    @recipe.ingredients.build
     # @recipe.ingredients.build(name: 'sugar', quantity: '1 cup')
     # @recipe.ingredients.build(name: 'flour', quantity: '2 cups')
   end
@@ -27,6 +27,7 @@ class RecipesController < ApplicationController
       render :new
     end
   end
+
 
   private
 
