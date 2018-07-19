f07fbc92b7eda4477f3e42ed7ee6ebb469ca4d00
Jul Jen <juliajen.prog@gmail.com>
2018-07-19 04:56:02 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,14 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
    accepts_nested_attributes_for :ingredients
+   before_save :erase_empty_ingredients
 
-   def ingredient_attributes=(ingredient)
-     self.ingredient = Ingredient.find_or_create_by(name: ingredient.name)
-     self.ingredient.update(ingredient)
-   end
+  def erase_empty_ingredients
+    self.ingredients = self.ingredients.select {|i| i.name && i.name != '' || i.quantity && i.quantity != ''}
+  end
+
+  def ingredient_attributes=(ingredient)
+   self.ingredient = Ingredient.find_or_create_by(name: ingredient.name)
+   self.ingredient.update(ingredient)
+  end
 end
