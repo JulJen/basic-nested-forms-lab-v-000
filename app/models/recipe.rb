115f816e95083b2f60da4b5a5d23397ed20c660a
Jul Jen <juliajen.prog@gmail.com>
2018-07-19 06:03:19 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,7 +6,7 @@ class Recipe < ActiveRecord::Base
     # self.ingredient = Ingredient.find_or_create_by(name: ingredient.name)
     # self.ingredient.update(ingredient)
     ingredient_attributes.each do |i, ingredient_attributes|
-      if ingredient.strip != ''
+      if ingredient.strip != nil
       self.ingredients.build(ingredient_attributes)
     end
   end
